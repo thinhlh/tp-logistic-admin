e74a436381ffd548736a14ba74512cc5b46b7a9d
Hoang Thinh <jonhnathankevin@yahoo.com.vn>
2023-03-19 00:12:36 +0700

    feat: login page

--- src/app/forgot-password/ForgotPassword.tsx
+++ src/app/forgot-password/ForgotPassword.tsx
@@ -19,7 +19,7 @@ export default function ForgotPassword() {
         <form className='flex flex-col w-full justify-evenly h-full items-center' onSubmit={handleSubmit}>
             <h1 className='text-5xl text-primary-color font-medium text-center'>FORGOT PASSWORD</h1>
             <p className="text-xl text-center w-2/3">{isSubmittedEmail ? `Please check your email` : `Please enter yout email to request a password reset.`} </p>
-            <input className='rounded-md w-full p-4 ring-1 ring-slate-500 text-xl' placeholder='Email' type="email" />
+            <input className='rounded-md w-full p-4 ring-1 ring-slate-500 text-xl' placeholder='Email' type="email" disabled={isSubmittedEmail} />
             <button className='rounded-md w-full  bg-primary-color disabled:bg-disabled-color px-4 py-4 ' type="submit" disabled={isSubmittedEmail}>
                 <p className='text-[#ffffff] font-semibold text-2xl'>RESET PASSWORD</p>
             </button>
